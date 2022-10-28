7165cf2e70b7ee13d971ed40df8f97dda760ec44
Simon <sema92m@wp.pl>
2022-10-28 21:13:48 +0200

    Add mobile version and AOS animatiom library

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log(1)
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle("open")) {
+		navBtnImg.src = '../../src/img/icons/nav-close.svg';
+    } else {
+		navBtnImg.src = '../../src/img/icons/nav-open.svg';
+    }
+};
+
+//animation library
+AOS.init({
+  disable: 'phone',
+  once: true
+});
